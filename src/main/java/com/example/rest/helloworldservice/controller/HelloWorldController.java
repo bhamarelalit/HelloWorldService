dbd10c721ba9832cca826dba656d4d1a90947ba1
Lalit <lalit_bhamare@yahoo.com>
2022-02-24 23:29:33 +0530

    Revert "Revert "First Commit""
    This reverts commit 19cd9e20f06d283e4f44d7c9f351687913cecbb1.

---
+++ src/main/java/com/example/rest/helloworldservice/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.rest.helloworldservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String Greeting()
+    {
+            return "HI from Rest Controller";
+    }
+
+
+}
