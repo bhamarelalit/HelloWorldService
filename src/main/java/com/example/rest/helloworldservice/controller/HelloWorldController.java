83e443d5e6de965e2c1ac3044d0b4926c5e02054
Lalit <lalit_bhamare@yahoo.com>
2022-03-10 23:03:59 +0530

    GN commit

--- src/main/java/com/example/rest/helloworldservice/controller/HelloWorldController.java
+++ src/main/java/com/example/rest/helloworldservice/controller/HelloWorldController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    String goodNight="GN";
+
     @GetMapping("/hello")
     public String Greeting()
     {
